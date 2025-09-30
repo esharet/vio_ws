d4fdc7d7106afd51b5aade276ddd54c789468d9e
bt <bt@gmail.com>
2025-09-30 09:54:39 +0300

    update dockerfile

--- env.sh
+++ env.sh
@@ -2,8 +2,8 @@
 # sudo sysctl -w net.ipv4.ipfrag_high_thresh=134217728
 # sudo sysctl -w net.core.rmem_max=2147483647
 
-# export RMW_IMPLEMENTATION=rmw_cyclonedds_cpp
-# export CYCLONEDDS_URI=file://$PWD/cyclonedds.xml
+# export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export CYCLONEDDS_URI=file://$PWD/cyclonedds.xml
 source /opt/ros/humble/setup.bash
 source install/setup.bash
 export PYTHONPATH=$PYTHONPATH:/workspace/submodules
